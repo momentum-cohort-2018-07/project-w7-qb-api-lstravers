cd9cbaa1515772ee2bb1c5d9b12265ad5adb7e5b
lstravers <stravers222@gmail.com>
2018-09-20 10:00:45 -0400

    modified application stylesheet, views, and user controller

--- Questionbox/app/controllers/api/users_controller.rb
+++ Questionbox/app/controllers/api/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::UsersController < ApplicationController
   
-  skip_before_action :verify_authenticity_token, only: [:create], raise: false
+  skip_before_action :verify_authenticity_token
   before_action :set_user, only: [:show, :destroy, :update]
 
   def index
@@ -14,12 +14,15 @@ class Api::UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(user_params)
+    @user = User.new(users_params)
     if @user.save
     render json: @user
     else
       render json: {"error": "Invalid"}, status: :unprocessable_entity
     end
+  end
+
+  def show
   end
 
 def destroy
